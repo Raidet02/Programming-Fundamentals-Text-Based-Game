ba8fc9acbb50d720bd1af41266eb5a8f7ecdb683
Raidet02 <146941600+Raidet02@users.noreply.github.com>
2023-11-13 21:44:39 +0000

    start to inventory

--- Programming Funadamentals Text Based Game/Inventory.h
+++ Programming Funadamentals Text Based Game/Inventory.h
@@ -14,16 +14,8 @@ void inventorySystem()
 
         playerCommand = playerCommandTemp.substr(0, end);
 
-        if (playerCommand == "items") //lists the item array
-        {
-            cout << endl << "> items:" << endl;
-
-            for (int i = 0; i < 5; i++)
-            {
-                cout << "- " << i << ": " << item[i] << endl;
-            }
-        }
-        else if (playerCommand == "show_all") //lists theplayers inventory
+        
+        if (playerCommand == "show_all") //lists theplayers inventory
         {
             cout << endl << "> inventory:" << endl;
             for (int i = 0; i < inventorySize; i++)
@@ -57,43 +49,6 @@ void inventorySystem()
                 else
                 {
                     cout << endl << "> Inventory Slot " << indexValue1 << " information:" << endl << "Name: " << inventory[indexValue1] << endl;
-                }
-            }
-        }
-        else if (playerCommand == "set") //changes the contents of an inventory slot to an item the player designates
-        {
-
-            indexValueS1 = playerCommandTemp.substr(end, stringSize - end); //gets the index values from the initail command
-
-            stringSize = sizeof(indexValueS1) / 4;
-            end = indexValueS1.find_last_of(" ");
-            indexValueS2 = indexValueS1.substr(end, stringSize - end);
-
-            validCheck = 0;
-
-            try //checks if the index value is valid
-            {
-                indexValue1 = stoi(indexValueS1);
-                indexValue2 = stoi(indexValueS2);
-            }
-            catch (...)
-            {
-                cout << endl << "Not a proper index" << endl;
-                validCheck = 1;
-            }
-
-            if (validCheck != 1) //if the index value is valid runs the code
-            {
-                indexValue1 = stoi(indexValueS1);
-                indexValue2 = stoi(indexValueS2);
-
-                if ((indexValue1 > inventorySize - 1 || indexValue1 < 0) && (indexValue2 > 4 || indexValue2 < 0))
-                {
-                    cout << "The index value does not exist" << endl;
-                }
-                else
-                {
-                    inventory[indexValue1] = item[indexValue2];
                 }
             }
         }
